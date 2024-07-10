89b0cc4df473b49b4b65c381504d5fc7bbc38bf0
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-07-10 15:38:43 +0545

    Pinned Icon Displayed

--- lib/widgets/task_list_item.dart
+++ lib/widgets/task_list_item.dart
@@ -44,78 +44,92 @@ class TaskListItem extends StatelessWidget {
           },
         );
       },
-      child: Card(
-        elevation: 2,
-        margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        child: ListTile(
-          leading: Checkbox(
-            value: task.isCompleted,
-            onChanged: task.isCompleted
-                ? null
-                : (bool? value) {
-                    Provider.of<TaskProvider>(context, listen: false)
-                        .toggleTaskCompletion(task.id);
-                  },
-          ),
-          title: Text(
-            task.title,
-            style: TextStyle(
-              decoration: task.isCompleted
-                  ? TextDecoration.lineThrough
-                  : TextDecoration.none,
-              decorationColor: task.isCompleted
-                  ? brightness == Brightness.light
-                      ? Utils.getCategoryColor(task.category).withOpacity(1)
-                      : CupertinoColors.black
-                  : null,
-              decorationThickness: 3,
-            ),
-          ),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(task.description),
-              SizedBox(height: 4),
-              Row(
+      child: Stack(
+        children: [
+          Card(
+            elevation: 2,
+            margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            child: ListTile(
+              leading: Checkbox(
+                value: task.isCompleted,
+                onChanged: task.isCompleted
+                    ? null
+                    : (bool? value) {
+                        Provider.of<TaskProvider>(context, listen: false)
+                            .toggleTaskCompletion(task.id);
+                      },
+              ),
+              title: Text(
+                task.title,
+                style: TextStyle(
+                  decoration: task.isCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                  decorationColor: task.isCompleted
+                      ? brightness == Brightness.light
+                          ? Utils.getCategoryColor(task.category).withOpacity(1)
+                          : CupertinoColors.black
+                      : null,
+                  decorationThickness: 3,
+                ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(CupertinoIcons.tag, size: 16),
-                  SizedBox(width: 4),
-                  Expanded(
-                    child: Text(
-                      task.category.isEmpty
-                          ? 'No category selected'
-                          : task.category,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  SizedBox(width: 8),
-                  Icon(CupertinoIcons.time, size: 16),
-                  SizedBox(width: 4),
-                  Expanded(
-                    child: Text(
-                      task.dueDate != null
-                          ? DateFormat('MMM d, y HH:mm').format(task.dueDate!)
-                          : 'No due date',
-                      overflow: TextOverflow.ellipsis,
-                    ),
+                  Text(task.description),
+                  SizedBox(height: 4),
+                  Row(
+                    children: [
+                      Icon(CupertinoIcons.tag, size: 16),
+                      SizedBox(width: 4),
+                      Expanded(
+                        child: Text(
+                          task.category.isEmpty
+                              ? 'No category selected'
+                              : task.category,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Icon(CupertinoIcons.time, size: 16),
+                      SizedBox(width: 4),
+                      Expanded(
+                        child: Text(
+                          task.dueDate != null
+                              ? DateFormat('MMM d, y HH:mm')
+                                  .format(task.dueDate!)
+                              : 'No due date',
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+              trailing: task.isCompleted
+                  ? null
+                  : IconButton(
+                      icon: Icon(CupertinoIcons.pencil),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) => EditTaskDialog(task: task),
+                        );
+                      },
+                    ),
+              tileColor: Utils.getCategoryColor(task.category).withOpacity(0.1),
+            ),
           ),
-          trailing: task.isCompleted
-              ? null
-              : IconButton(
-                  icon: Icon(CupertinoIcons.pencil),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => EditTaskDialog(task: task),
-                    );
-                  },
-                ),
-          tileColor: Utils.getCategoryColor(task.category).withOpacity(0.1),
-        ),
+          if (task.isPinned)
+            Positioned(
+              top: 8,
+              left: 8,
+              child: Icon(
+                CupertinoIcons.pin_fill,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+            ),
+        ],
       ),
     );
   }
